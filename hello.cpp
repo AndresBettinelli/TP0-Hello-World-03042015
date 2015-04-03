24064d24bfb04f45b396dbd4d366cfaddbcb2e2f
Andres Bettinelli <bettinelli.andres@gmail.com>
2015-04-03 01:56:19 -0300

    TP0 Helo World
    Andres Bettinelli - K1091

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+/* Tp#0 Hello World!
+Andres Bettinelli - K1091
+Ultima modificacion 03/04/2015
+*/
+
+#include<iostream>
+
+using namespace std; 
+int main ()
+{
+cout<<"Hola Mundo!\n";
+system("pause");
+return 0;
+}
